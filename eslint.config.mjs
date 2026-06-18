6bd1fa6abc582b97fbdfd2e26276620e72555228
Wesley McGhee <wesmcghee6372@gmail.com>
2026-06-18 10:52:26 -0500

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...compat.extends("eslint-config-next/core-web-vitals"),
+  ...compat.extends("eslint-config-next/typescript"),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
